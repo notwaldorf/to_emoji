7a59d487b03e763251398bd06566e12e31c6e4a3
Monica Dinculescu <noms@google.com>
2017-06-09 20:38:47 -0700

    omg omg omg it tweets

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # to_emoji
-🤖✨ Twitter bot that translates every tweet you send it to emoji 
+🤖✨ Twitter bot that translates every tweet you send it to emoji
+
+
+## Usage
+Populate the `.env` file like this:
+```
+module.exports = {
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  ''
+}
+```
---
+++ bot.js
@@ -0,0 +1,40 @@
+const translate = require('moji-translate');
+const Twit = require('twit');
+const T    = new Twit(require('./.env'));
+const stream = T.stream('user');
+
+// Thanks to @ohhoe's https://github.com/rachelnicole/isReallyCute-bot for
+// all of the code <3
+
+// Send a welcome message to a follower.
+stream.on('follow', function (event) {
+  let source = event.source;
+  let screenName = source.screen_name;
+
+  var welcome  = '👋 🆒 🐱! Tweet something at me and I\'ll traslate it to ✨🎉💰';
+
+  console.log('👋 ' + screenName);
+
+  T.post('statuses/update', {
+      status: '@' + screenName + ' ' + welcome
+    }, function(err, data, response) {
+    // console.log(data);
+  })
+})
+
+// Translate a tweet.
+stream.on('tweet', function (message) {
+	let screenName = message.user.screen_name;
+  let nameID = message.id_str;
+
+  let text = message.text;
+  let translated = translate.translate(text, true);
+	console.log(screenName, text, translated);
+
+	if (message.in_reply_to_screen_name === 'to_emoji') {
+		T.post('statuses/update', { in_reply_to_status_id: nameID, status: '@' + screenName + ' ' + translated }, function(err, data, response) {
+			// console.log(data)
+		})
+	}
+
+})
