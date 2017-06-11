a2847fd263c1ee68285e6889d713f4d2bafb392f
Monica Dinculescu <noms@google.com>
2017-06-10 19:58:26 -0700

    update env

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ const T = new Twit({
   consumer_key:        process.env.EMOJI_BOT_CONSUMER_KEY || config.consumer_key,
   consumer_secret:     process.env.EMOJI_BOT_CONSUMER_SECRET || config.consumer_secret,
   access_token:        process.env.EMOJI_BOT_ACCESS_TOKEN || config.access_token,
-  access_token_secret: process.env.EMOJI_BOT_ACCESS_TOKEN_SECRET || config.access_token_secret,
+  access_token_secret: process.env.EMOJI_BOT_ACCESS_SECRET || config.access_token_secret,
   timeout_ms:          60*1000,  // optional HTTP request timeout to apply to all requests.
 });
 
